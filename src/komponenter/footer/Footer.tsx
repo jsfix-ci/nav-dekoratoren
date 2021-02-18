dfeb310d5cccc77e3e4c08e67089f67bd5e78136
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-02-18 20:03:45 +0100

    lagt til id paa footer, som mountpoint for modalen

--- src/komponenter/footer/Footer.tsx
+++ src/komponenter/footer/Footer.tsx
@@ -4,17 +4,15 @@ import { AppState } from 'store/reducers';
 import SimpleFooter from './footer-simple/FooterSimple';
 import FooterRegular from './footer-regular/FooterRegular';
 import './Footer.less';
+import Utloggingsvarsel from '../common/utloggingsvarsel/Utloggingsvarsel';
 
 const Footer = () => {
     const { PARAMS } = useSelector((state: AppState) => state.environment);
     return (
-        <div className={'decorator-wrapper'}>
-            <footer className="sitefooter">
-                {PARAMS.SIMPLE || PARAMS.SIMPLE_FOOTER ? (
-                    <SimpleFooter />
-                ) : (
-                    <FooterRegular />
-                )}
+        <div id="decorator-wrapper-footer" className={'decorator-wrapper'}>
+            <Utloggingsvarsel />
+            <footer id="sitefooter" className="sitefooter">
+                {PARAMS.SIMPLE || PARAMS.SIMPLE_FOOTER ? <SimpleFooter /> : <FooterRegular />}
             </footer>
         </div>
     );
