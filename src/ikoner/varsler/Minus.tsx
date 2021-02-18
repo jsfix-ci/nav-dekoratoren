eaa15435d317dfda3257c7fe4001e1c9b60f1114
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-02-18 20:04:15 +0100

    lagt til ikoner som brukes i modalen

---
+++ src/ikoner/varsler/Minus.tsx
@@ -0,0 +1,9 @@
+import React, { SVGProps } from 'react';
+
+const Minus = (props: SVGProps<SVGSVGElement>) => (
+    <svg width={props.width} height={props.height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" clipRule="evenodd" d="M21 13H3v-2h18v2z" fill="#000000" />
+    </svg>
+);
+
+export default Minus;
